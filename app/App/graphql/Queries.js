cd4d5ea20f143282d4d61dbdf5bfc7f07196c75f
Domenico Colandrea <dcolandrea@rcanalytics.com>
2020-04-23 00:21:04 -0400

    cleanup

--- app/App/graphql/Queries.js
+++ app/App/graphql/Queries.js
@@ -2,13 +2,13 @@ import gql from 'graphql-tag';
 
 export const Questions = gql`
 	query AllQuestions {
-		questions @rest(type: "QuestionsPayload", path: "getQuestions") {
-			BunionSurgery @type(name: "BunionSurgery") {
+		questions @rest(type: "Questions", path: "getQuestions") {
+			BunionSurgery @type(name:"bunion") {
 				question
 				category
 				answers
 			}
-			DigitalSurgery @type(name: "DigitalSurgery") {
+			DigitalSurgery @type(name: "digital") {
 				question
 				category
 				answers
